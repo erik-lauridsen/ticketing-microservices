394ea2e137df75311a2d2f656918729372e0d468
Erik Lauridsen <elauridsen03@gmail.com>
2025-11-09 11:36:16 -0500

    Trying out jetstream to replace deprecated node-nats-streaming

---
+++ jetstream-test/src/nats-publisher.ts
@@ -0,0 +1,23 @@
+import { JetStreamClient } from "nats";
+import { Subjects } from "./subjects";
+import { streamName } from "./stream-name";
+
+
+interface Event {
+    subject:Subjects;
+    data:any;
+}
+
+export abstract class Publisher<T extends Event>{
+    abstract subject: T['subject'];
+    protected jsClient: JetStreamClient; 
+
+    constructor(jsClient: JetStreamClient) { 
+        this.jsClient = jsClient;
+    }
+
+    async publish(data: T['data']): Promise<void> {
+        await this.jsClient.publish(`${this.subject}`, JSON.stringify(data));
+        console.log('Event published to subject:', this.subject);
+    }
+}
